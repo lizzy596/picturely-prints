4671b0fbd5835aacb49dca84db0b96f1605da2e0
lizzy596 <lizmeek123@gmail.com>
2022-05-29 21:30:04 -0500

    resized rating component

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -7,11 +7,11 @@ const Rating = ({value, text}) => {
   return (
     <div className="rating">
     <div>
-    <span>{value >= 1 ? <ImStarFull style={style} size={24} /> : value >= 0.5 ? <ImStarHalf  style={style} size={24}/> : <ImStarEmpty  style={style} size={24} />}</span>
-    <span>{value >= 2 ? <ImStarFull style={style} size={24} /> : value >= 1.5 ? <ImStarHalf  style={style} size={24}/> : <ImStarEmpty  style={style} size={24} />}</span>
-    <span>{value >= 3 ? <ImStarFull style={style} size={24} /> : value >= 2.5 ? <ImStarHalf  style={style} size={24}/> : <ImStarEmpty  style={style} size={24} />}</span>
-    <span>{value >= 4 ? <ImStarFull style={style} size={24} /> : value >= 3.5 ? <ImStarHalf  style={style} size={24}/> : <ImStarEmpty  style={style} size={24} />}</span>
-    <span>{value >= 5 ? <ImStarFull style={style} size={24} /> : value >= 4.5 ? <ImStarHalf  style={style} size={24}/> : <ImStarEmpty  style={style} size={24} />}</span>
+    <span>{value >= 1 ? <ImStarFull style={style} size={20} /> : value >= 0.5 ? <ImStarHalf  style={style} size={20}/> : <ImStarEmpty  style={style} size={20} />}</span>
+    <span>{value >= 2 ? <ImStarFull style={style} size={20} /> : value >= 1.5 ? <ImStarHalf  style={style} size={20}/> : <ImStarEmpty  style={style} size={20} />}</span>
+    <span>{value >= 3 ? <ImStarFull style={style} size={20} /> : value >= 2.5 ? <ImStarHalf  style={style} size={20}/> : <ImStarEmpty  style={style} size={20} />}</span>
+    <span>{value >= 4 ? <ImStarFull style={style} size={20} /> : value >= 3.5 ? <ImStarHalf  style={style} size={20}/> : <ImStarEmpty  style={style} size={20} />}</span>
+    <span>{value >= 5 ? <ImStarFull style={style} size={20} /> : value >= 4.5 ? <ImStarHalf  style={style} size={20}/> : <ImStarEmpty  style={style} size={20} />}</span>
     </div>
     <div>
     <span className="rating-text">{text}</span>
